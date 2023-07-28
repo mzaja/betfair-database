a70286c578cd55f7d017a2b06654aa5edd9058ce
mzaja <64711255+mzaja@users.noreply.github.com>
2023-07-28 18:46:44 +0100

    #13 First stab at CI pipeline

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-@REM black --check tests betfairdatabase
+black --check betfairdatabase tests
 coverage run --source=betfairdatabase -m unittest discover -s tests -b
 coverage html
 coverage report

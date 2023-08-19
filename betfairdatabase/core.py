713a0cccf42285162929c79ef13417b358131aba
mzaja <64711255+mzaja@users.noreply.github.com>
2023-08-19 12:57:09 +0100

    Prototype insert function

--- betfairdatabase/core.py
+++ betfairdatabase/core.py
@@ -1,8 +1,11 @@
 import contextlib
 import csv
+import datetime as dt
 import json
+import shutil
 import sqlite3
 from pathlib import Path
+from typing import Callable
 
 INDEX_FILENAME = ".betfairdatabaseindex"
 DATA_FILE_SUFFIXES = ("", ".zip", ".gz", ".bz2")
@@ -69,6 +72,7 @@ def parse_market_catalogue(market_catalogue_file: str | Path) -> dict:
 
 def flatten_subdict(parent_dict: dict[str, object], child_key: str) -> None:
     """
+    Flattens a dictionary by combining parent and child's key names.
     Modifies the dictionary in place.
     """
     if subdict := parent_dict.pop(child_key, None):
@@ -120,6 +124,22 @@ def create_sql_mapping(
     return sql_data_map
 
 
+def parse_data_and_insert_row(
+    connection: sqlite3.Connection,
+    market_catalogue_file: str | Path,
+    market_data_file: str | Path,
+):
+    """
+    Parses the market catalogue file, transforms the data into an SQL table row and
+    imports the said row into an SQL table using the provided connection object.
+    """
+    sql_data_map = create_sql_mapping(market_catalogue_file, market_data_file)
+    connection.execute(
+        f"INSERT INTO {SQL_TABLE_NAME} VALUES ({','.join('?'*len(sql_data_map))})",
+        tuple(sql_data_map.values()),
+    )
+
+
 def construct_index(database_dir: str | Path) -> int:
     """
     Constructs the database index (in the form of another database) from source files
@@ -131,16 +151,9 @@ def construct_index(database_dir: str | Path) -> int:
     ) as conn, conn:
         conn.execute(f"CREATE TABLE {SQL_TABLE_NAME}({','.join(SQL_TABLE_COLUMNS)})")
         for market_catalogue_file in locate_market_catalogues(database_dir):
-            for suffix in DATA_FILE_SUFFIXES:
-                data_file = market_catalogue_file.with_suffix(suffix)
-                if data_file.exists():
-                    sql_data_map = create_sql_mapping(market_catalogue_file, data_file)
-                    conn.execute(
-                        f"INSERT INTO {SQL_TABLE_NAME} VALUES ({','.join('?'*len(sql_data_map))})",
-                        tuple(sql_data_map.values()),
-                    )
-                    data_files_indexed += 1
-                    break
+            if data_file := locate_market_data_file(market_catalogue_file):
+                parse_data_and_insert_row(conn, market_catalogue_file, data_file)
+                data_files_indexed += 1
             else:
                 # Log warning that a data file is missing
                 pass
@@ -192,3 +205,98 @@ def export_data_to_csv(data: list[dict], output_file: str | Path):
             writer = csv.DictWriter(f, data[0].keys())
             writer.writeheader()
             writer.writerows(data)
+
+
+def parse_datetime(datetime_str: str) -> dt.datetime:
+    """Parses Betfair's ISO 8601 datetime format."""
+    try:
+        # Python >= 3.11 parses timezone
+        return dt.datetime.fromisoformat(datetime_str)
+    except ValueError:
+        # Python 3.10 does not, so remove "Zulu" time marker from the end
+        return dt.datetime.fromisoformat(datetime_str.replace("Z", ""))
+
+
+def locate_market_data_file(market_catalogue_file: str | Path) -> Path | None:
+    """
+    Locates the market data file, which is expected to be next to the
+    market catalogue file and share the same basename.
+    """
+    for suffix in DATA_FILE_SUFFIXES:
+        data_file = Path(market_catalogue_file).with_suffix(suffix)
+        if data_file.exists():
+            return data_file
+    return None  # Market data file was not found
+
+
+class ImportPatterns:
+    """
+    Contains patterns for mapping market catalogue data to the output
+    directory path in the database.
+    """
+
+    @staticmethod
+    def betfair_historical(market_catalogue_data: dict) -> str:
+        """
+        Generates the output directory path using the pattern
+        "{year_no}/{month_name}/{day_no}/{event_id}"
+        e.g. "2022/Jun/06/3828473", where reference time is "marketStartTime".
+        This pattern is how official Betfair historical data files are organised.
+
+        NOTE:
+        The actual naming pattern Betfair uses is slightly different as the reference
+        time is the event end time, rather than the market start time.
+        If an event takes place across 4th and 5th of February 2022, all markets will
+        be stored inside "2022/Feb/5/{event_id}", rather than a split of ".../Feb/4/..."
+        and ".../Feb/5/...". However, since the event end time is not a part of the
+        market catalogue, it is impossible to fully recreate this pattern.
+        """
+        market_time = parse_datetime(market_catalogue_data["marketStartTime"])
+        event_id = market_catalogue_data["event"]["id"]
+        return market_time.strftime(f"%Y/%b/{market_time.day}/{event_id}")
+
+    @staticmethod
+    def event_id(market_catalogue_data: dict) -> str:
+        """Market data is stored into directories named after event ids."""
+        return market_catalogue_data["event"]["id"]
+
+    @staticmethod
+    def flat(market_catalogue_data: dict) -> str:
+        """Market data is stored directly into the base directory."""
+        return ""
+
+
+def insert(
+    database_dir: str | Path,
+    source_dir: str | Path,
+    copy: bool = False,
+    pattern: Callable[[dict], str] = ImportPatterns.betfair_historical,
+):
+    """
+    If copy is True, copies the files instead of moving them.
+    """
+    with contextlib.closing(
+        sqlite3.connect(construct_index_path(database_dir))
+    ) as conn, conn:
+        for market_catalogue_file in locate_market_catalogues(source_dir):
+            if data_file := locate_market_data_file(market_catalogue_file):
+                output_dir = Path(database_dir) / pattern(
+                    parse_market_catalogue(market_catalogue_file)
+                )
+                market_catalogue_dest_file = output_dir / market_catalogue_file.name
+                market_data_dest_file = output_dir / data_file.name
+                if (
+                    market_catalogue_dest_file.exists()
+                    or market_data_dest_file.exists()
+                ):
+                    # Do not allow replacing existing files at the moment because that
+                    # requires updating the SQL table
+                    continue
+                else:
+                    # Copy or move the files to the destination
+                    operation = shutil.copy if copy else shutil.move
+                    operation(market_catalogue_file, market_catalogue_dest_file)
+                    operation(data_file, market_data_dest_file)
+                    parse_data_and_insert_row(
+                        conn, market_catalogue_dest_file, market_data_dest_file
+                    )

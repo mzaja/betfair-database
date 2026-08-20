1a5ac898b83ea5a1e95e41b7858662cd2b05e031
mzaja <64711255+mzaja@users.noreply.github.com>
2026-08-20 16:57:00 +0100

    #33 Add corrupt data file parsing unit tests

--- tests/test_directory.py
+++ tests/test_directory.py
@@ -161,16 +161,13 @@ class DatabaseDirectoryTests(unittest.TestCase):
     def test_do_not_merge_metadata(self):
         """
         Tests the legacy behaviour where individual market metadata files are left alone.
-        However, metadata.json is still generated if
+        However, metadata.json is still generated if some data files are not covered.
         """
         with TestFixture(
             Datasets(uncompressed=True, official=True, bulk_metadata=True), flatten=True
         ) as temp_dir:
             dd = DatabaseDirectory(temp_dir, *self.categorise_files(temp_dir))
             src_metadata_files = dd.individual_metadata_files
-            src_metadata_lookup = {
-                data[MARKET_ID]: data for data in map(read_json, src_metadata_files)
-            }
             src_bulk_metadata_lookup = dd.bulk_metadata_file.parse_and_validate()
             data_file_market_ids = set(
                 DatabaseDirectory._get_market_id(f) for f in dd.data_files
@@ -235,7 +232,7 @@ class DatabaseDirectoryInvalidFilesTests(unittest.TestCase):
         self._fixture.close()
 
     def test_individual_metadata_file_is_corrupt(self):
-        """Tests attempting to parse a metadata.json file which is not valid JSON."""
+        """Tests parsing a market metadata file which is not valid JSON."""
         meta_file = self.temp_dir / "1.11112222.json"
         meta_file.write_text("{")
         dd = DatabaseDirectory(self.temp_dir, individual_metadata_files=[meta_file])
@@ -248,7 +245,7 @@ class DatabaseDirectoryInvalidFilesTests(unittest.TestCase):
         self.assertRegex(error_messages[0], f"Error parsing.*{meta_file.name}")
 
     def test_individual_metadata_file_is_not_a_dict(self):
-        """Tests attempting to parse a metadata.json file which is not valid JSON."""
+        """Tests parsing a market metadata file which is not a dict."""
         meta_file = self.temp_dir / "1.22223333.json"
         meta_file.write_text("[]")
         dd = DatabaseDirectory(self.temp_dir, individual_metadata_files=[meta_file])
@@ -264,7 +261,7 @@ class DatabaseDirectoryInvalidFilesTests(unittest.TestCase):
         )
 
     def test_individual_metadata_file_does_not_contain_a_market_id_field(self):
-        """Tests attempting to parse a metadata.json file which is not valid JSON."""
+        """Tests parsing a market metadata.json file which is missing a market ID field."""
         meta_file = self.temp_dir / "1.33334444.json"
         meta_file.write_text("{}")
         dd = DatabaseDirectory(self.temp_dir, individual_metadata_files=[meta_file])
@@ -277,4 +274,37 @@ class DatabaseDirectoryInvalidFilesTests(unittest.TestCase):
         self.assertRegex(
             error_messages[0],
             f"{meta_file.name}.*does not contain valid market metadata",
+        )
+
+    def test_market_data_file_is_corrupt(self):
+        """Tests parsing corrupt market data files."""
+        workdir = self.temp_dir / "corrupt"
+        Datasets(corrupt=True, missing_metadata=True).copy_files(workdir, flatten=True)
+        data_files = [workdir / "1.221089567.zip", workdir / "1.223716890"]
+        dd = DatabaseDirectory(workdir, data_files=data_files)
+
+        with self.assertLogs(level=logging.ERROR) as logs:
+            self.assertEqual(dd._parse_metadata(set()), {})
+
+        error_messages = get_error_messages(logs)
+        self.assertEqual(len(error_messages), 2)
+
+        for msg, file in zip(error_messages, data_files):
+            self.assertRegex(msg, f"Error parsing.*{file.name}")
+
+    def test_market_data_file_contains_no_metadata(self):
+        """Tests parsing a valid market data file without metadata."""
+        # workdir = self.temp_dir / "missing_metadata"
+        # Datasets(missing_metadata=True).copy_files(workdir, flatten=True)
+        file = self.temp_dir / "1.44445555.json"
+        file.write_text("{}\n{}\n{}")
+        dd = DatabaseDirectory(self.temp_dir, data_files=[file])
+
+        with self.assertLogs(level=logging.ERROR) as logs:
+            self.assertEqual(dd._parse_metadata(set()), {})
+
+        error_messages = get_error_messages(logs)
+        self.assertEqual(len(error_messages), 1)
+        self.assertRegex(
+            error_messages[0], f"Market definition missing in.*{file.name}"
         )
